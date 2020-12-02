3e1829ac75ac6494fd4f9547f01414fb1a96a472
Denis Grafov <grafov.denis@gmail.com>
2020-12-02 18:39:53 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'views/echotest_view.dart';
+import 'views/pubsub_view.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(GetMaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(context) => Scaffold(
+      appBar: AppBar(title: Text("ion flutter examples")),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          RaisedButton(
+              child: Text("Echo Test"),
+              onPressed: () {
+                Get.to(EchoTestView(), transition: Transition.rightToLeft);
+              }),
+          RaisedButton(
+              child: Text("Pub/Sub"),
+              onPressed: () {
+                Get.to(PubSubTestView(), transition: Transition.rightToLeft);
+              }),
+        ],
+      )));
+}
